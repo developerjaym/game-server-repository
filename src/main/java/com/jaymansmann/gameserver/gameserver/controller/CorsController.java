d19deb9e5730143dec53e83d7eb428218ae4f5e1
Jay Mansmann <jaymansmann@yahoo.com>
2022-04-13 22:14:35 -0400

    Cors pass through

---
+++ src/main/java/com/jaymansmann/gameserver/gameserver/controller/CorsController.java
@@ -0,0 +1,39 @@
+package com.jaymansmann.gameserver.gameserver.controller;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("/cors")
+@RestController
+public class CorsController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping()
+	public ResponseEntity<?> readMessagesFrom(@RequestParam("url") String url){
+		ResponseEntity<String> original = restTemplate.getForEntity(url, String.class);
+		MultiValueMap<String, String> newHeaders = new LinkedMultiValueMap<>(original.getHeaders());
+		newHeaders.put(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, Arrays.asList("*"));
+		newHeaders.put(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, Arrays.asList("*"));
+		ResponseEntity<String> response2 = new ResponseEntity<>(original.getBody(), newHeaders, original.getStatusCode());
+		return response2;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder().build();
+	}
+}
